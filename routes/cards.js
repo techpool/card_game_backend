ae366768de13e0f9fd85e9dd5e1ecc9e01660ad5
techpool <suryadeep10@gmail.com>
2017-06-02 12:19:34 +0530

    card fetch functions made modular

--- routes/cards.js
+++ routes/cards.js
@@ -1,78 +1,142 @@
-var express = require('express');
-var router = express.Router();
-var Cards = require('../models/Cards');
+const express = require('express');
+const router = express.Router();
+const Cards = require('../models/Cards');
 
-/* GET users listing. */
 
-router.post('/', function (req, res, next) {
-
+/**
+ * 
+ * Creates or updates a card, if not created then a new one is created
+ * 
+ * @param  {String}   userId   User Id of the card
+ * @param  {Object}   data     Data object
+ *                             {
+ *                                 type: <String>,
+ *                                 cardId: <String>
+ *                             }
+ * @param  {Function} callback Callback function to handle the response of db
+ * 
+ */
+function createOrUpdateCard(userId, data, callback) {
     Cards.findOne({
-        userId: req.user._id
+        userId: userId
     }, function (cardFetchError, fetchedCard) {
         if (cardFetchError) {
             res.status(500).json(cardFetchError);
             return;
         } else if (!fetchedCard) {
 
-            var cardObj = new Cards();
-
-            console.log(typeof String(req.user._id))
-            cardObj[req.body.type] = 1;
-            cardObj.addedCards = [req.body.cardId];
-            cardObj['userId'] = req.user._id;
+            let cardObj = new Cards();
+            cardObj[data.type] = 1;
+            cardObj.addedCards = [data.cardId];
+            cardObj['userId'] = userId;
             cardObj.save(function (cardSaveError, savedCard) {
                 if (cardSaveError) {
-                    res.status(500).json(cardSaveError);
-                    return;
+                    callback({
+                        status: 500,
+                        error: cardSaveError
+                    });
                 } else {
-                    res.status(201).json(savedCard);
+                    callback(null, savedCard);
                 }
             })
         } else {
 
-            var currentValue = fetchedCard[req.body.type] || 0;
-            fetchedCard[req.body.type] = currentValue + 1;
-            fetchedCard.addedCards.push(req.body.cardId);
+            var currentValue = fetchedCard[data.type] || 0;
+            fetchedCard[data.type] = currentValue + 1;
+            fetchedCard.addedCards.push(data.cardId);
             fetchedCard.save(function (cardUpdateError, updatedCard) {
-                res.status(201).json(updatedCard);
+                callback(null, updatedCard);
                 return;
             });
         }
     });
-});
+}
 
-router.get('/', function (req, res, next) {
+/**
+ * 
+ * Function to fetch card of a particular user
+ * 
+ * @param  {String}   userId   User ID of the person whose cards needs to be fetched
+ * @param  {Function} callback Callback function to handle the response data from db
+ * 
+ */
+function fetchCard(userId, callback) {
     Cards.findOne({
-        userId: req.user._id
+        userId: userId
     }, function (cardFetchError, fetchedCard) {
         if (cardFetchError) {
-            res.status(500).json(cardFetchError);
-            return;
+            callback({
+                status: 500,
+                error: cardFetchError
+            });
         } else if (!fetchedCard) {
-            res.status(404).json({
+            callback({
+                status: 404,
                 info: 'No cards found'
             });
         } else {
+            callback(null, fetchedCard);
+        }
+    });
+}
 
-            res.status(200).json(fetchedCard);
+/**
+ * 
+ * Function to delete a card of a user
+ * 
+ * @param  {String}   userId   User ID whose card needs to be deleted
+ * @param  {Function} callback Callback function to handle response from database
+ */
+function deleteCard(userId, callback) {
+    Cards.findOne({
+        userId: userId
+    }, function (cardFetchError, fetchedCard) {
+        if (cardFetchError) {
+            callback({
+                status: 500,
+                error: cardFetchError
+            });
+        } else if (!fetchedCard) {
+            callback({
+                status: 404,
+                info: 'No cards found'
+            });
+        } else {
+            fetchedCard.remove();
+            callback(null, {
+                info: 'Deleted Successfully'
+            });
+        }
+    });
+}
+
+
+router.post('/', function (req, res, next) {
+    createOrUpdateCard(req.user._id, req.body, function(error, data) {
+        if (error) {
+            res.status(error.status || 500).json(error);
+        } else {
+            res.status(201).json(data);
+        }
+    })
+});
+
+router.get('/', function (req, res, next) {
+    fetchCard(req.user._id, function(error, data) {
+        if (error) {
+            res.status(error.status || 500).json(error);
+        } else {
+            res.status(200).json(data);
         }
     });
 });
 
 router.delete('/', function (req, res, next) {
-    Cards.findOne({
-        userId: req.user._id
-    }, function (cardFetchError, fetchedCard) {
-        if (cardFetchError) {
-            res.status(500).json(cardFetchError);
-            return;
-        } else if (!fetchedCard) {
-            res.status(404).json({
-                info: 'No cards found'
-            });
+    deleteCard(req.user._id, function(error, data) {
+        if (error) {
+            res.status(error.status || 500).json(error);
         } else {
-            fetchedCard.remove();
-            res.status(200).json(fetchedCard);
+            res.status(200).json(data);
         }
     });
 });
